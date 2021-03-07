e7b35f044fa6e04974546e4f574ed293fecf668b
Zubair shah <65024163+zubair894@users.noreply.github.com>
2021-03-07 12:00:04 +0500

    header making

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <div>
+            <header>
+                <h1>
+                    Expense Tracker by zubair shaikh
+                </h1>
+            </header>
+        </div>
+    )
+}
+export default Header;
