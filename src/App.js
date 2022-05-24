31d58dd8aad667879f1bfd0823af55ce3f6826a1
zubair-shah <shahzubair269@gmail.com>
2022-05-24 20:57:26 +0500

    edit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-
-import './App.css';
-import Header from './components/Header'
-import Balance from './components/Balance'
-import AccountSummary from './components/AccountSummary';
-import { TransactionHistory } from './components/TransactionHistory';
+import "./App.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import AccountSummary from "./components/AccountSummary";
+import { TransactionHistory } from "./components/TransactionHistory";
 // import {Accounthistory} from './components/Accounthistory'
-import {AddTransaction} from './components/Addtransection'
+import { AddTransaction } from "./components/Addtransection";
 // // import {TransactionContext} from './components/Transcontext'
- import {GlobalProvider} from './components/context/GlobalState';
+import { GlobalProvider } from "./components/context/GlobalState";
 
 function App() {
   return (
-   <div>
-
-<GlobalProvider>
-    
-            <Header />
-    <div className="container-main">
-      <div className="main-box">
-      <Balance />
-      <AccountSummary />
-      <TransactionHistory />
-      <AddTransaction />
-
-      </div>
-        
+    <div>
+      <GlobalProvider>
+        <Header />
+        <div className="container-main">
+          <div className="main-box">
+            <Balance />
+            <AccountSummary />
+            <TransactionHistory />
+            <AddTransaction />
+          </div>
+        </div>
+      </GlobalProvider>
     </div>
-    </GlobalProvider>
-    </div>
-
   );
 }
 
